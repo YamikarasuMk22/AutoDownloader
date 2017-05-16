91190b5adadd4f5f7445863e2b6aa1e4a2002594
J34568 <J34568@VE1W7-05-002.opec.jbcc.co.jp>
2017-05-16 17:38:06 +0900

    changed

---
+++ src/com/adl/downloader/DownloadManager.java
@@ -0,0 +1,21 @@
+package com.adl.downloader;
+
+import java.net.URL;
+
+public class DownloadManager {
+	//マックスのページ数分ダウンロードする
+	public static void downloadImgs(URL rootUrl, int maxPage) {
+
+	}
+
+	//ランダムでインターバルを置く(bot偽装)
+	public static void getRandInterval() {
+
+	}
+
+	//作品を保存するフォルダーの作成
+	public static String createDownloadFolder() {
+		return null;
+
+	}
+}
--- src/com/adl/test/ModuleTest.java
+++ src/com/adl/test/ModuleTest.java
@@ -17,5 +17,4 @@ public class ModuleTest {
 		System.out.println(contentType);
 
 	}
-
 }
