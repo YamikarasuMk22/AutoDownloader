6cbebdaff6fef5ca6a9e13c4492b32bf98bd729b
J34568 <J34568@VE1W7-05-002.opec.jbcc.co.jp>
2017-06-29 17:55:18 +0900

    Create XMLReader

--- src/com/adl/test/ModuleTest.java
+++ src/com/adl/test/ModuleTest.java
@@ -1,26 +1,33 @@
 package com.adl.test;
 
 import java.io.IOException;
+import java.util.List;
 
 import com.adl.constant.ConnectionConstants;
 import com.adl.srcreader.ProxyConfig;
-import com.adl.webdriver.WDProcessor;
+import com.adl.xml.XMLReader;
 
 public class ModuleTest implements ConnectionConstants {
 
 	public static void main(String[] args) throws IOException {
 		ProxyConfig.proxyAccess(true, false);
 
+		List<String> list = XMLReader.readComicXMLParams("id");
+
+		for(int i=0; i<list.size(); i++) {
+			System.out.println(i + ":" + list.get(i));
+		}
+
 
 //		WDProcessor.processGallery();
 //
-		try {
-			WDProcessor.createWebDriver();
-			WDProcessor.processDownloadImg("1068078" ,"1068078", "png", 2);
-
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+//		try {
+//			WDProcessor.createWebDriver();
+//			WDProcessor.processDownloadImg("1068078" ,"1068078", "png", 2);
+//
+//		} catch (Exception e) {
+//			e.printStackTrace();
+//		}
 
 //		String src = WebDriver.getSrc(DOWNLOAD_ROOT_URL);
 //
--- src/com/adl/xml/XMLReader.java
+++ src/com/adl/xml/XMLReader.java
@@ -3,42 +3,68 @@ package com.adl.xml;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
 public class XMLReader extends XMLEditor {
 
 	/**
 	 * comicのパラメータを全て取得する
 	 */
-	public static List<String> readComicParam(String id) {
-		List<String> list = new ArrayList<String>();
-		return list;
+	public static List<String> readComicXMLParams(String id) {
+		List<String> strList = new ArrayList<String>();
+
+		try {
+			Document document = getRootDocument(XML_COMICS);
+			Element root = document.getDocumentElement();
+
+			NodeList list = root.getElementsByTagName("Comic");
+			for (int i = 0; i < list.getLength(); i++) {
+				Element element = (Element) list.item(i);
+				String strSN = element.getAttribute("id");
+				if (strSN.compareTo(id) == 0) {
+					NodeList nodeList = element.getChildNodes();
+
+					for (int j=0; j < nodeList.getLength(); j++) {
+						Node comicNode = nodeList.item(j);
+						if (comicNode.getNodeType() == Node.ELEMENT_NODE) {
+							strList.add(comicNode.getTextContent());
+						}
+					}
+				}
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return strList;
 	}
 
-	public static String readTitle(String id) {
-		return id;
-	}
+	/**
+	 * comicの指定したID・Tagのパラメータを取得する
+	 */
+	public static String readComicXMLParam(String id, String Tag) {
+		String str = "";
 
-	public static String readTitleJapanese(String id) {
-		return id;
-	}
+		try {
+			Document document = getRootDocument(XML_COMICS);
+			Element root = document.getDocumentElement();
 
-	public static String readOriginTitle(String id) {
-		return id;
-	}
-
-	public static String readCharaList(String id) {
-		return id;
-	}
-
-	public static String readTagList(String id) {
-		return id;
-	}
-
-	public static String readArtist(String id) {
-		return id;
-	}
-
-	public static String readPageNum(String id) {
-		return id;
+			NodeList list = root.getElementsByTagName("Comic");
+			for (int i = 0; i < list.getLength(); i++) {
+				Element element = (Element) list.item(i);
+				String strSN = element.getAttribute("id");
+				if (strSN.compareTo(id) == 0) {
+					NodeList titleList = element.getElementsByTagName(Tag);
+					Element titleElement = (Element)titleList.item(0);
+					str = titleElement.getFirstChild().getNodeValue();
+				}
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return str;
 	}
 
 	/**
